58f5196aa5907f858bafa8f1c47e475634db0f62
Alex Pikalov <alex.pikalov.khar@gmail.com>
2023-11-07 22:00:57 +0100

    Implement shape_vctop_half_shape instructions

--- rasmus/src/execute/mod.rs
+++ rasmus/src/execute/mod.rs
@@ -71,12 +71,13 @@ use self::exec_vec::{
 };
 use self::exec_vector::{
     all_true_16x8, all_true_32x4, all_true_64x2, all_true_8x16, bitmask_16x8, bitmask_32x4,
-    bitmask_64x2, bitmask_8x16, f32x4_vcvtop_i32x4, i16_extend_i32_s, i16_extend_i32_u,
-    i16x8_vcvtop_half_i8x16, i32_convert_f64_u, i32_extend_i64_s, i32_extend_i64_u,
-    i32x4_vcvtop_f32x4, i32x4_vcvtop_half_i16x8, i64x2_vcvtop_half_i32x4, i8_extend_i16_s,
-    i8_extend_i16_u, shape_16x8_narrow_32x4_s, shape_16x8_narrow_32x4_u, shape_8x16_narrow_16x8_s,
-    shape_8x16_narrow_16x8_u, shape_f32_convert_i32_s, shape_f32_convert_i32_u,
-    shape_i32_trunc_f32_s, shape_i32_trunc_f32_u, Half, i32_convert_f64_s,
+    bitmask_64x2, bitmask_8x16, f32_promote_f64, f32x4_vcvtop_i32x4, i16_extend_i32_s,
+    i16_extend_i32_u, i16x8_vcvtop_half_i8x16, i32_convert_f64_s, i32_convert_f64_u,
+    i32_extend_i64_s, i32_extend_i64_u, i32x4_vcvtop_f32x4, i32x4_vcvtop_half_i16x8,
+    i64x2_vcvtop_half_i32x4, i8_extend_i16_s, i8_extend_i16_u, shape_16x8_narrow_32x4_s,
+    shape_16x8_narrow_32x4_u, shape_8x16_narrow_16x8_s, shape_8x16_narrow_16x8_u,
+    shape_f32_convert_i32_s, shape_f32_convert_i32_u, shape_i32_trunc_f32_s, shape_i32_trunc_f32_u,
+    Half,
 };
 
 #[allow(dead_code)]
@@ -507,7 +508,11 @@ pub fn execute_instruction(
         }
         InstructionType::F64x2ConvertLowI32x4S => {
             i64x2_vcvtop_half_i32x4(stack, i32_convert_f64_s, Half::Low)?
-        } // _ => unimplemented!(),
+        }
+        InstructionType::F64x2PromoteLowF32x4 => {
+            i64x2_vcvtop_half_i32x4(stack, f32_promote_f64, Half::Low)?
+        }
+        // _ => unimplemented!(),
     }
 
     Ok(())
